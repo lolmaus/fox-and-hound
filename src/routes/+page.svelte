9b087af750d5c50ab9efcb23f0e1f370fb61dfbd
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2023-10-27 18:39:47 +0300

    Add Vercel Postgres

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,17 @@
+<script lang="ts">
+  export let data;
+</script>
+
 <h1>Welcome to SvelteKit!</h1>
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>
+  {#await data.streamed.views}
+    Loading view count...
+  {:then views}
+    This page has been viewed {views} times.
+  {:catch error}
+    Something wrong happened: {error.message}
+  {/await}
+</p>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { loadEnv } from 'vite';
+import dotenvExpand from 'dotenv-expand';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+export default defineConfig(({ mode }) => {
+	// https://github.com/vercel/storage/tree/main/packages/postgres#a-note-for-vite-users
+	if (mode === 'development') {
+		const env = loadEnv(mode, process.cwd(), '');
+		dotenvExpand.expand({ parsed: env});
 	}
+
+	return {
+		plugins: [sveltekit()],
+		test: {
+			include: ['src/**/*.{test,spec}.{js,ts}'],
+		},
+	};
 });
